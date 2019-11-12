cf012268a1de0df9cd3784b924faa3e25eedcf6c
Maxim Pirozhkov <pirozhkovmaxim@gmail.com>
2019-11-12 13:26:41 +0600

    Add User page

---
+++ src/Components/UserPage/UserPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Button from '@material-ui/core/Button';
+
+export default class UserPageComponent extends React.Component {
+
+
+    render() {
+        return (
+            <h1>
+                This is user page
+            </h1>
+        );
+    }
+}
